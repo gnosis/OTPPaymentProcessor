09e93071334f7b277d523677eb38bb4a7cb957fa
Auryn Macmillan <hi@auryn.xyz>
2022-08-16 22:45:27 +1000

    Setup project and a first pass at contract updates

--- contracts/ProcessrorSingleUser.sol
+++ contracts/ProcessrorSingleUser.sol
@@ -1,3 +1,7 @@
+// SPDX-License-Identifier: LGPL-3.0-only
+
+/// @title TangemPaymentProcessor -- A contract which allows a given payment processor to process user-authorized ERC20 token transfers.
+
 /*  
 This is the simplest possible implementation for a single user:
 processor is only involved in processing of transactions coming from Visa,
@@ -8,6 +12,10 @@ In the multi-user scenario, a separate set of all variables should be stored for
 The user shall APPROVE this contract as a SPENDER from the host ERC20 contract.
 */
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+pragma solidity >=0.7.0 <0.9.0;
+
 contract TangemTokenWallet {
     uint32 public sequence;
     uint16 public otpRootCounter;
@@ -15,45 +23,68 @@ contract TangemTokenWallet {
     uint256 public spendLimit;
     address card; // mapping to this address in the multi-user scenario
     address processor; // in this simple implementation: same for all users, cannot be updated once the contract is deployed
-    ERC20Interface tokenContract;
+    ERC20 token;
 
-    constructor(
-        address card_,
-        address processor_,
-        address contractAddress,
-        uint256 spendLimit_
-    ) {
-        card = card_;
-        processor = processor_;
-        tokenContract = ERC20Interface(contractAddress);
-        spendLimit = spendLimit_;
+    // Only callable by card
+    error OnlyCard(address card, address sender);
+
+    // Only callable by processor
+    error OnlyProcessor(address processor, address sender);
+
+    // Amount exceeds spend limit
+    error ExceedsSpendLimit(uint256 limit, uint256 amount);
+
+    // Invalid counter
+    error InvalidCounter(uint256 optRootCounter, uint256 counter);
+
+    // Invalid OTP
+    error InvalidOtp(bytes16 otp);
+
+    // Transfer Failed
+    error TransferFailed();
+
+    modifier onlyCard() {
+        if (msg.sender != card) revert OnlyCard(card, msg.sender);
+        _;
     }
 
-    //  Sync OTP between the card wallet and the applet (to be called at least once in the beginning)
+    modifier onlyProcessor() {
+        if (msg.sender != processor)
+            revert OnlyProcessor(processor, msg.sender);
+        _;
+    }
 
-    function initOTP(bytes16 otp, uint16 counter) external {
-        require(msg.sender == card, "Invalid sender!"); // signed by the user only
+    constructor(
+        address _card,
+        address _processor,
+        address _token,
+        uint256 _spendLimit
+    ) {
+        card = _card;
+        processor = _processor;
+        token = ERC20(_token);
+        spendLimit = _spendLimit;
+    }
+
+    // Sync OTP between the card wallet and the applet (to be called at least once in the beginning)
+    function initOTP(bytes16 otp, uint16 counter) external onlyCard {
         otpRoot = otp;
         otpRootCounter = counter;
     }
 
-    //  Collecting funds from Visa payments
-
+    // Collecting funds from Visa payments
     function process(
         uint256 tokenAmount,
         bytes16 otp,
         uint16 counter
-    ) public {
-        require(msg.sender == processor, "Invalid sender!");
-        require(tokenAmount <= spendLimit, "Amount exceeds spend limit!");
-        require(
-            tokenAmount <= tokenContract.balanceOf(address(this)),
-            "Insufficient balance!"
-        );
-        require(otpRootCounter > counter, "Invalid counter!");
+    ) external onlyProcessor {
+        if (tokenAmount > spendLimit)
+            revert ExceedsSpendLimit(spendLimit, tokenAmount);
 
-        //  Authorization - OTP received from the card in the Visa tx is verified against the next OTP in the smart-contract
+        if (otpRootCounter < counter)
+            revert InvalidCounter(otpRootCounter, counter);
 
+        // Authorization - OTP received from the card in the Visa tx is verified against the next OTP in the smart-contract
         bytes20 bAddress = bytes20(card);
         bytes16 bOTP = otp;
         uint16 c = counter;
@@ -61,19 +92,16 @@ contract TangemTokenWallet {
             bOTP = bytes16(sha256(bytes.concat(bAddress, bytes2(c), bOTP)));
             c++;
         }
-        require(bOTP == otpRoot, "Invalid OTP!");
+        if (bOTP != otpRoot) revert InvalidOtp(bOTP);
         otpRootCounter = counter;
         otpRoot = otp;
-        require(
-            tokenContract.transfer(processor, tokenAmount),
-            "Transfer failed!"
-        );
+        bool success = token.transferFrom(card, processor, tokenAmount);
+        if (!success) revert TransferFailed();
     }
 
     // User sets the max amount of a single tx that can be approved by the processor in the PROCESS method (above)
 
-    function changeSpendLimit(uint256 spendLimit) external {
-        require(msg.sender == card, "Invalid sender!");
-        spendLimit = spendLimit_;
+    function changeSpendLimit(uint256 _spendLimit) external onlyCard {
+        spendLimit = _spendLimit;
     }
 }
