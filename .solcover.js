932121aa995be943642186f9d672d775d843003d
Auryn Macmillan <hi@auryn.xyz>
2022-08-18 01:04:31 +1000

    add coverage

---
+++ .solcover.js
@@ -0,0 +1,9 @@
+module.exports = {
+  skipFiles: [
+    "test/"
+  ],
+  mocha: {
+    grep: "@skip-on-coverage", // Find everything with this tag
+    invert: true, // Run the grep's inverse set.
+  },
+};
